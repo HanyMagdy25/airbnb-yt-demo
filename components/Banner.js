43c70a3ecd6163ee883cb5d2d3f100045bb75fee
HanyMagdy25 <hany.magdy25@gmail.com>
2022-12-27 03:54:31 +0200

    first commit

--- components/Banner.js
+++ components/Banner.js
@@ -7,7 +7,7 @@ function Banner() {
     <div className="relative h-[300px] sm:h-[400px] lg:h-[500px] xl:h-[600px] 2xl:h-[700px]">
       <Image src={bannerImg} fill alt="banner" objectFit="cover" />
       <div className="absolute top-1/2 w-full text-center">
-        <p className="text-sm sm:text-lg ">Not sure where to go ? Perfect.</p>
+        <p className="text-sm sm:text-lg text-white drop-shadow-2xl">Not sure where to go ? Perfect.</p>
         <button className="text-purple-500 bg-white px-10 py-4 shadow-md rounded-full font-bold my-3 hover:shadow-xl active:scale-90 transition duration-150">
           I&apos;m flexible
         </button>
